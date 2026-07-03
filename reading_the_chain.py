6b6c2f8691723ccd1af322788750961377883aee
fly238 <itiswellobi@gmail.com>
2026-07-03 14:53:19 -0400

    Updated reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -19,19 +19,19 @@ def connect_to_eth():
 
 
 def connect_with_middleware(contract_json):
-	# TODO insert your code for this method from last week's assignment
 	with open(contract_json, "r") as f:
 		d = json.load(f)
 		d = d['bsc']
 		address = d['address']
 		abi = d['abi']
-
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
 	url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
 	w3 = Web3(HTTPProvider(url))
 	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+
+	contract = w3.eth.contract(address=address, abi=abi)
+
 	return w3, contract
 
 
